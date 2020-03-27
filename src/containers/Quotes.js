579025091e69e5937d30c5c3c888d3bb74c06930
tylergreason <tyler.greason@protonmail.com>
2020-03-27 11:53:14 -0400

    Done.

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { upvoteQuote } from '../actions/quotes';
+import { downvoteQuote } from '../actions/quotes';
+import { removeQuote } from '../actions/quotes';
 
 class Quotes extends Component {
 
+  renderQuotes = (quotes) => {
+    return quotes.map(quote =>{
+      return <QuoteCard 
+        quote={quote}
+        key={quote.id}
+        upvoteQuote={this.props.upvoteQuote}
+        downvoteQuote = {this.props.downvoteQuote}
+        removeQuote = {this.props.removeQuote}
+        />
+    })
+  }
   render() {
     return (
       <div>
@@ -15,11 +29,8 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
-
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+              {/*TODO: Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes*/}
+              {this.renderQuotes(this.props.quotes)}  
             </div>
           </div>
         </div>
@@ -27,6 +38,8 @@ class Quotes extends Component {
     );
   }
 }
-
+const mapStateToProps = state => {
+  return { quotes: state.quotes }
+}
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(mapStateToProps, { upvoteQuote,downvoteQuote, removeQuote })(Quotes);
